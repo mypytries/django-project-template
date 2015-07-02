997b8eaadc0ffbd3eaf4f4a6bedec02db6915e7c
mypytries <mypytries@mail.ru>
2015-07-02 09:00:39 +0300

    add students dict and modify students_list.html

--- students/views.py
+++ students/views.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+	    {'id': 1,
+	    'first_name': u'Валентин',
+	    'last_name': u'Петренко',
+	    'ticket': 5141,
+	    'image': 'img/1.jpg'},
+	    {'id': 2,
+	    'first_name': u'Павло',
+	    'last_name': u'Рудий',
+	    'ticket': 5181,
+	    'image': 'img/2.jpg'},
+	    {'id': 1,
+	    'first_name': u'Віталій',
+	    'last_name': u'Семеной',
+	    'ticket': u'5141M',
+	    'image': 'img/3.jpg'},
+	)
+    return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
     return render(request, '<h1>Student Add Form</h1>')
@@ -28,3 +47,4 @@ def groups_edit(request, gid):
 
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
+
